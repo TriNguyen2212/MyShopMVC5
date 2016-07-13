e99e38056c55f8e0c0d0603cb3116aea65d23aea
TriNguyen2212 <nguyenvantri.cntt@gmail.com>
2016-07-13 23:46:44 +0700

    The first commit

---
+++ myShop.Web/Global.asax.cs
@@ -0,0 +1,28 @@
+﻿using myShop.Web.Mappings;
+using Newtonsoft.Json;
+using System;
+using System.Web.Http;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace myShop.Web
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            AutoMapperConfiguration.Configure();
+            //fix bug serialize for web API
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Serialize;
+            GlobalConfiguration.Configuration.Formatters.Remove(GlobalConfiguration.Configuration.Formatters.XmlFormatter);
+
+            JsonSerializerSettings jSettings = new JsonSerializerSettings();
+            jSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings = jSettings;
+        }
+    }
+}
---
+++ myShop.Web/Models/TagViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace myShop.Web.Models
+{
+    public class TagViewModel
+    {
+        public string ID { set; get; }
+        public string Name { set; get; }
+        public string Type { set; get; }
+    }
+}
