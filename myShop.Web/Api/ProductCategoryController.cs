d8db4e1c4f37d769ac0c5ec3a2456a292a60ebe5
Nguyễn Văn Trí <nguyenvantri.cntt@gmail.com>
2016-07-19 01:55:29 +0700

    add more list data

---
+++ myShop.Web/Api/ProductCategoryController.cs
@@ -0,0 +1,37 @@
+﻿using myShop.Web.Infratructure.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MyClassShop.Service;
+using AutoMapper;
+using MyClassShop.Model.Models;
+using myShop.Web.Models;
+
+namespace myShop.Web.Api
+{
+    [RoutePrefix("api/productcategory")]
+    public class ProductCategoryController : ApiControllerBase
+    {
+        IProductCategoryService _productCategoryService;
+
+        public ProductCategoryController(IErrorService errorService,IProductCategoryService productCategoryService) 
+            : base(errorService)
+        {
+            this._productCategoryService = productCategoryService;
+        }
+        [Route("getall")]
+        public HttpResponseMessage GetAll(HttpRequestMessage request)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                var model = _productCategoryService.GetAll();
+                var responseData = Mapper.Map<IEnumerable<ProductCategory>, IEnumerable<ProductCategoryViewModel>>(model);
+                var response = request.CreateResponse(HttpStatusCode.OK, responseData);
+                return response;
+            });
+        }
+    }
+}
