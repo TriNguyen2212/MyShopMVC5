4f263d4176907b5c4d6ade8c822b29f98b1fdbc2
Nguyễn Văn Trí <nguyenvantri.cntt@gmail.com>
2016-07-18 22:53:56 +0700

    route implement for app

---
+++ myShop.Web/app/components/products/products.module.js
@@ -0,0 +1,19 @@
+﻿/// <reference path="/Assets/admin/libs/angular/angular.js" />
+
+(function () {
+    angular.module('tedushop.products', ['tedushop.commom']).config(config);
+
+    config.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+    function config($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('products', {
+            url: "/products",
+            templateUrl: "app/components/products/productListView.html",
+            controller: "productListController"
+        }).state('product_Add', {
+            url: "/product_Add",
+            templateUrl: "app/components/products/productAddView.html",
+            controller: "productAddController"
+        });
+    }
+})();
---
+++ myShop.Web/app/shared/modules/teduShop.common.js
@@ -0,0 +1,3 @@
+﻿(function () {
+    angular.module('tedushop.commom', ['ui.router']);
+})();
